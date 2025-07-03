4821dd956f5194e861dd13cd192aaae68c53eae1
Gabriel <gabrielcilches123@gmail.com>
2025-07-03 03:35:35 -0400

    ADD: Meta nivel, modelo bote separado, sfx vela

---
+++ Proyecto_Barco/Assets/Gabriel/5.Scripts/Test_Controles_Barco/Logica_NIveles/MetaNivel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MetaNivel : MonoBehaviour
+{
+    [Header("Nombre de la escena de ramificación")]
+    public string escenaRamificacion = "run_mundo1"; // ← cámbialo desde el Inspector si usas otra escena
+
+    [Header("Efectos opcionales")]
+    public GameObject efectoFinalNivel; // (opcional) Partículas, animación, etc.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Nivel completado, regresando a escena de ramificación...");
+
+            // Activar efecto visual (si hay uno asignado)
+            if (efectoFinalNivel != null)
+                Instantiate(efectoFinalNivel, transform.position, Quaternion.identity);
+
+            // Cargar escena de ramificación
+            SceneManager.LoadScene(escenaRamificacion);
+        }
+    }
+}
